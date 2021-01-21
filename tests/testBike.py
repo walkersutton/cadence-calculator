428f6611314452545eaf0022f97c77756715fdcc
walkersutton <walker.c.sutton@gmail.com>
2021-01-21 01:43:13 -0500

    unittesting? good idea

---
+++ tests/testBike.py
@@ -0,0 +1,21 @@
+# created by walker
+# https://walkercsutton.com
+
+import unittest
+import sys
+# sys.path.insert(1, '../calculator')
+
+from haversine import Unit
+from calculator import cadence, distance, speed, TIMEUNIT
+
+
+class TestBike(unittest.TestCase):
+	def setUp(self):
+		self.cog = 16
+		self.chainring = 48
+		self.tire_width = 25
+		self.wheel_diameter = 622
+
+
+if __name__ == '__main__':
+    unittest.main()
