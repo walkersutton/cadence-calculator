0a9cbcd46727df1e132b0c8604bae9f1650ccf96
walkersutton <walker.c.sutton@gmail.com>
2021-09-07 00:32:35 -0400

    gpx stubbing

--- gpx.py
+++ gpx.py
@@ -13,3 +13,29 @@ def generateCadenceElement(cadence):
 	cadence_element = etree.Element("cadence")
 	cadence_element.text = cadence
 	return cadence_element
+
+def create_gpx(stream):
+	""" Creates a GPX file with this stream's data
+
+		Args:
+			stream (obj)
+
+		TODO
+
+		Returns a filename to the newly created gpx file
+		TODO tar compress before creating post request
+	"""
+
+"""
+NOTES
+* look into python gpx validation (or maybe just xml)
+  * this was reccomended somewhere https://xerces.apache.org/xerces-c/
+* Library for creating GPX easily
+  * https://github.com/tkrajina/gpxpy
+  * https://pypi.org/project/gpxpy/
+  * https://github.com/kosmo/gpx-from-strava
+* https://www.topografix.com/gpx.asp
+* https://stackoverflow.com/questions/68457696/strava-api-to-export-gpx-file-of-activity
+
+"""
+
