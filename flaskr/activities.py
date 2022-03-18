488904512e29e5b30eebb36b3cc3cf51bc8a887c
walkersutton <walker.c.sutton@gmail.com>
2022-03-17 23:59:14 -0400

    moving selenium logic to separate module

--- flaskr/activities.py
+++ flaskr/activities.py
@@ -2,17 +2,11 @@ import logging
 import time
 
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.support.ui import WebDriverWait as wait
-from selenium.webdriver.support.expected_conditions import alert_is_present
-from selenium.common.exceptions import NoSuchElementException
 
 from flaskr import config
 from flaskr.gpx import create_gpx
 from flaskr.cadence import generate_cadence_data
+from flaskr.webdriver import delete_activity
 
 
 class Activity:
@@ -219,19 +213,16 @@ class Activity:
     def delete_activity(self) -> bool:
         ''' Deletes the given activity_id
 
-        Args:
-            athlete_id:
-                the id of the athlete
-            activity_id:
-                the id of the activity
         Returns:
             Whether or not this activity was deleted successfully
+            TODO - maybe instead of depending on return value here, we listen for a post DELETE? - seems better
         '''
         athlete_id = self.obj['athlete']['id']
         activity_id = self.obj['id']
         logging.info('beginning to delete activity')
         # TODO
         # grab email/passwords from storage , ooooooooof
+        
         try:
             if athlete_id == int(config.TEST_ATHLETE_ID):
                 email = config.TEST_ATHLETE_EMAIL
@@ -248,44 +239,8 @@ class Activity:
         if not(email and password):
             logging.error('email or password was not set')
             return False
-        try:
-            service = Service(config.CHROMEDRIVER_PATH)
-            options = webdriver.ChromeOptions()
-            options.binary_location = config.GOOGLE_CHROME_BIN
-            options.add_argument('--headless')
-            options.add_argument('--disable-dev-shm-usage')
-            options.add_argument('--no-sandbox')
-            driver = webdriver.Chrome(service=service, options=options)
-            driver.get('https://www.strava.com/login')
-            driver.find_element(By.ID, value='email').send_keys(email)
-            driver.find_element(By.ID, value='password').send_keys(password)
-            driver.find_element(By.ID, 'login-button').click()
-            # TODO
-            # verify user is logged in - can check for the existence of a class in body/root tag
-            # driver.find_element(By.XPATH, value='//button[@class="btn-accept-cookie-banner"]').click() - pretty sure this is unnecessary
-            driver.get(f'https://www.strava.com/activities/{activity_id}')
-            driver.find_element(
-                By.XPATH, value='//div[@title="Actions"]').click()
-            driver.find_element(
-                By.XPATH, value='//a[@data-method="delete"][text()[contains(.,"Delete")]]').click()
-            wait(driver, 3).until(alert_is_present())
-            Alert(driver).accept()
-            time.sleep(3)
-            driver.quit()
-            logging.info('successfully deleted the activity')
-            return True
-            # TODO
-            # make a call to the strava api and see if the activity is still available
-        except NoSuchElementException as e:
-            logging.error('Element not found:')
-            logging.error(e)
-            driver.quit()
-            return False
-        except Exception as e:
-            logging.error('Generic Selenium exception:')
-            logging.error(e)
-            driver.quit()
-            return False
+
+        return delete_activity(email, password, activity_id)
 
     def upload_activity(self, data_type: str, external_id: str, file_path: str) -> int:
         ''' Uploads an Activity
